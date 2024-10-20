472c308e576fe87b387bfc10f75a4b1d09b99e08
cianci0 <116454811+cianci0@users.noreply.github.com>
2024-10-20 23:13:25 +0300

    Update radar.py

--- radar.py
+++ radar.py
@@ -8,9 +8,6 @@ def radarchart(values, average_values, output_file='static/radar_chart.svg'):
     angles = [n / float(num_vars) * 2 * pi for n in range(num_vars)]
     angles += angles[:1]
 
-    values += values[:1]
-    average_values += average_values[:1]
-
     fig = figure.Figure(figsize=(7, 7))
     ax = fig.add_subplot(111, polar=True)
 
@@ -23,10 +20,16 @@ def radarchart(values, average_values, output_file='static/radar_chart.svg'):
     ax.set_yticklabels([])
     ax.set_ylim(0, 100)
 
-    ax.plot(angles, average_values, linewidth=0, linestyle='solid', color='lightgray')
-    ax.fill(angles, average_values, 'lightgray', alpha=1)
+    if average_values:
+        print(len(average_values), len(values))
+        
+        average_values += average_values[:1]
+        ax.plot(angles, average_values, linewidth=0, linestyle='solid', color='lightgray')
+        ax.fill(angles, average_values, 'lightgray', alpha=1)
 
-    ax.plot(angles, values, linewidth=1, linestyle='solid', color='#ffd500')
-    ax.fill(angles, values, '#ffd500', alpha=0.5)
+    if values:
+        values += values[:1]
+        ax.plot(angles, values, linewidth=1, linestyle='solid', color='#ffd500')
+        ax.fill(angles, values, '#ffd500', alpha=0.5)
 
     fig.savefig(output_file, format='svg')
