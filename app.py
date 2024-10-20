226692ad54764506db7c7d159812daf4c9668e8e
cianci0 <116454811+cianci0@users.noreply.github.com>
2024-10-20 23:12:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,55 @@ from radar import radarchart
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///<käyttäjänimi>"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///ciancio"
 db = SQLAlchemy(app)
+
+def populate_tables():
+        regions = {
+            1: "Italy",
+            2: "Spain",
+            3: "Greece",
+            4: "Portugal",
+            5: "Turkey",
+            6: "European Union",
+            7: "Other",
+            8: "Tunisia",
+            9: "France"
+        }
+
+        producers = {
+            1: ["Desantis", "De Cecco", "Carapelli", "Monini", "Agrinatura",
+                "Del Console", "Pirkka", "Frantoi Cutrera", "Turri", "Pinzimolio",
+                "Änglamark", "Bartolini Emilio", "Corazza", "Capannino",
+                "Terre Francescane", "Olitalia", "Ranise", "Lidl", "Frantoio D’Orazio"
+            ],
+            8: ["Urtekram"],
+            2: ["Finca La Gramanosa", "Borges", "Pirkka", "Coop", "Änglamark",
+                "Herkku", "Pons", "Menu", "Lidl", "Golden Star",
+                "Marqués de Valdueza", "Oro del Desierto"],
+            3: ["Fontana", "Filos", "Urtekram", "Pirkka", "Memmas Knossos",
+                "Terra Creta", "Gaea", "Änglamark", "Iliada", "Biolea",
+                "Lidl", "Greenolia", "Oleosophia", "Foodin"],
+            6: ["Nicolas Alziari"],
+            9: ["Chateau Virant"]
+        }
+
+        def insert_producer(producer_name, region_id):
+            sql = text("INSERT INTO producers (producer_name, region_id) VALUES (:producer_name, :region_id)")
+            db.session.execute(sql, {"producer_name": producer_name, "region_id": region_id})
+            db.session.commit()
+
+        def insert_region(region_name):
+            sql = text("INSERT INTO regions (region_name) VALUES (:region_name)")
+            db.session.execute(sql, {"region_name": region_name})
+            db.session.commit()
+
+        for region_id, region_name in regions.items():
+            insert_region(region_name)
+
+        for region_id, producer_list in producers.items():
+            for producer_name in producer_list:
+                insert_producer(producer_name, region_id)
 
 def login_required(f):
     @wraps(f)
@@ -109,7 +156,6 @@ def rate():
         sql = text("""
             INSERT INTO oils (oil_name, producer_id, region_id)
             VALUES (:oil_name, :producer_id, :region_id)
-            ON CONFLICT (oil_name, producer_id, region_id) DO NOTHING
             RETURNING oil_id
         """)
 
@@ -141,16 +187,8 @@ def rate():
         sql = text("""
             INSERT INTO ratings (user_id, oil_id, rating, fruity, grassy, salty, sweet, floral, pungent, citrusy)
             VALUES (:user_id, :oil_id, :rating, :fruity, :grassy, :salty, :sweet, :floral, :pungent, :citrusy)
-            ON CONFLICT (user_id, oil_id) DO UPDATE SET
-                rating = EXCLUDED.rating,
-                fruity = EXCLUDED.fruity,
-                grassy = EXCLUDED.grassy,
-                salty = EXCLUDED.salty,
-                sweet = EXCLUDED.sweet,
-                floral = EXCLUDED.floral,
-                pungent = EXCLUDED.pungent,
-                citrusy = EXCLUDED.citrusy
         """)
+        
         db.session.execute(sql, {
             "user_id": user_id,
             "oil_id": oil_id,
@@ -192,26 +230,61 @@ def rating(oil_id):
     }
 
     sql = text("""
-        SELECT oils.oil_name, producers.producer_name, regions.region_name, ratings.rating
-        FROM oils
-        JOIN producers ON oils.producer_id = producers.producer_id
-        JOIN regions ON oils.region_id = regions.region_id
-        JOIN ratings ON oils.oil_id = ratings.oil_id
-        WHERE oils.oil_id = :oil_id AND ratings.user_id = :user_id
+    SELECT oils.oil_name, regions.region_name, producers.producer_name, 
+           ratings.rating, ratings.fruity, ratings.grassy, 
+           ratings.salty, ratings.sweet, ratings.floral, 
+           ratings.pungent, ratings.citrusy 
+    FROM ratings
+    JOIN oils ON ratings.oil_id = oils.oil_id
+    JOIN producers ON oils.producer_id = producers.producer_id
+    JOIN regions ON oils.region_id = regions.region_id
+    WHERE oils.oil_id = :oil_id
+    """)
+    
+    result = db.session.execute(sql, {"oil_id": oil_id})
+    ratings = [list(row) for row in result.fetchall()]
+    print(ratings)
+    oil_name, oil_region, oil_producer = ratings[0][0:3]
+
+    sql = text("""
+    SELECT rating, fruity, grassy, salty, sweet, floral, pungent, citrusy 
+    FROM ratings 
+    WHERE oil_id = :oil_id AND user_id = :user_id
     """)
     
     result = db.session.execute(sql, {"oil_id": oil_id, "user_id": session.get("user_id")})
-    oil = result.fetchone()
+    user_rating = result.fetchone()
 
-    if oil:
-        oil_name, oil_producer, oil_region, oil_rating = oil
-        country_code = country_codes.get(oil_region, "un")
-        return render_template(
-            "rating.html", oil_name=oil_name, oil_producer=oil_producer, 
-            oil_region=oil_region, oil_rating=oil_rating, country_code=country_code
-        )
+    ratings_array = array([row[3:] for row in ratings])
+    if len(ratings) > 1:
+        averages = mean(ratings_array, axis=0).tolist()
     else:
-        return redirect("/your-oils")
+        averages = ratings[0][3:]
+
+    if user_rating:
+        values = list(user_rating)
+        oil_rating = values.pop(0)
+    else:
+        values = []
+        oil_rating = None
+
+    print(values, averages)
+    average_rating = averages.pop(0)
+    print(values, averages)
+    radarchart(values=values, average_values=averages)
+
+    country_code = country_codes.get(oil_region, "un")
+    
+    return render_template(
+        "rating.html", 
+        oil_name=oil_name, 
+        oil_producer=oil_producer, 
+        oil_region=oil_region, 
+        oil_rating=oil_rating, 
+        country_code=country_code, 
+        average_rating=round(average_rating, 2)
+    )
+
 
 @app.route("/your-oils", methods=["GET"])
 @login_required
@@ -235,7 +308,21 @@ def your_oils():
 @app.route("/best-oils")
 @login_required
 def best_oils():
-    return render_template("best-oils.html")
+    sql = text("""
+        SELECT oils.oil_id, oils.oil_name, producers.producer_name, regions.region_name, 
+               AVG(ratings.rating) as average_rating
+        FROM oils
+        JOIN producers ON oils.producer_id = producers.producer_id
+        JOIN regions ON oils.region_id = regions.region_id
+        JOIN ratings ON oils.oil_id = ratings.oil_id
+        GROUP BY oils.oil_id, producers.producer_name, regions.region_name
+        ORDER BY average_rating DESC
+    """)
+    
+    result = db.session.execute(sql)
+    oils = result.fetchall()
+    
+    return render_template("best-oils.html", oils=oils)
 
 @app.route("/logout")
 @login_required
@@ -281,3 +368,8 @@ def get_ratings(oil_id):
         averages = []
 
     return jsonify(averages)
+
+if __name__ == "__main__":
+    with app.app_context():
+        populate_tables()
+        print("Tables populated succesfully")
